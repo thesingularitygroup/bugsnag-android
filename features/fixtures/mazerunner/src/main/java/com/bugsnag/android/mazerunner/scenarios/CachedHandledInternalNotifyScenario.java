666ee850e0199f3decf259c5c2e33a9f32052e96
Delisa Mason <iskanamagus@gmail.com>
2019-08-28 18:12:56 +0100

    tests: Assert custom exceptions are serialized correctly

---
+++ features/fixtures/mazerunner/src/main/java/com/bugsnag/android/mazerunner/scenarios/CachedHandledInternalNotifyScenario.java
@@ -0,0 +1,40 @@
+package com.bugsnag.android.mazerunner.scenarios;
+
+import android.app.Activity;
+import android.content.Context;
+
+import com.bugsnag.android.mazerunner.CustomSerializedException;
+import com.bugsnag.android.Bugsnag;
+import com.bugsnag.android.Configuration;
+
+import java.util.HashMap;
+
+public class CachedHandledInternalNotifyScenario extends Scenario {
+
+    private boolean shouldTestOfflineCaching = false;
+
+    public CachedHandledInternalNotifyScenario(Configuration config, 
+                                               Context context) {
+        super(config, context);
+        config.setAutoCaptureSessions(false);
+        Activity activity = (Activity)context;
+        String state = activity.getIntent().getStringExtra("EVENT_METADATA");
+        shouldTestOfflineCaching = state != null && state.equals("offline");
+        if (shouldTestOfflineCaching) {
+            disableAllDelivery(config);
+        }
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        if (shouldTestOfflineCaching) {
+            Bugsnag.internalClientNotify(new CustomSerializedException(),
+                    new HashMap<String, Object>() {{
+                        put("severity", "warning");
+                        put("severityReason", "handledException");
+                    }}, false, null);
+        }
+    }
+}
+
