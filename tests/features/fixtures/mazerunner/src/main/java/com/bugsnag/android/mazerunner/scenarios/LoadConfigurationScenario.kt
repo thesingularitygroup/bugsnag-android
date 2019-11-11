2550a2de2bc7d00abb343628650cb24bfc78fff7
Alex Moinet <alex@bugsnag.com>
2019-11-11 11:34:56 +0000

    Test immutable config: Fix build issue

--- tests/features/fixtures/mazerunner/src/main/java/com/bugsnag/android/mazerunner/scenarios/LoadConfigurationScenario.kt
+++ tests/features/fixtures/mazerunner/src/main/java/com/bugsnag/android/mazerunner/scenarios/LoadConfigurationScenario.kt
@@ -5,6 +5,7 @@ import com.bugsnag.android.Configuration
 import com.bugsnag.android.mazerunner.IgnorableException
 
 import android.content.Context
+import android.content.Intent
 
 /**
  * Sends a handled exception to Bugsnag, which does not include session data.
