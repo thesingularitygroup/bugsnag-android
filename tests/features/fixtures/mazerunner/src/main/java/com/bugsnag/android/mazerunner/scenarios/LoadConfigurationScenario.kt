bbf543dfc402cc60d15f147458a03aa60e9dd4f7
Alex Moinet <alex@bugsnag.com>
2019-11-12 11:18:14 +0000

    Test immutable config: Add a sleep and flush to ensure payload order

--- tests/features/fixtures/mazerunner/src/main/java/com/bugsnag/android/mazerunner/scenarios/LoadConfigurationScenario.kt
+++ tests/features/fixtures/mazerunner/src/main/java/com/bugsnag/android/mazerunner/scenarios/LoadConfigurationScenario.kt
@@ -2,10 +2,14 @@ package com.bugsnag.android.mazerunner.scenarios
 
 import com.bugsnag.android.Bugsnag
 import com.bugsnag.android.Configuration
+import com.bugsnag.android.flushAllSessions
 import com.bugsnag.android.mazerunner.IgnorableException
 
 import android.content.Context
 import android.content.Intent
+
+import android.os.Handler
+import android.os.HandlerThread
 
 /**
  * Sends a handled exception to Bugsnag, which does not include session data.
@@ -13,19 +17,31 @@ import android.content.Intent
 internal class LoadConfigurationScenario(config: Configuration,
                                         context: Context) : Scenario(config, context) {
 
+    companion object {
+        private const val SLEEP_MS: Long = 300
+    }
+
     override fun run() {
         super.run()
-        context.startActivity(Intent("com.bugsnag.android.mazerunner.UPDATE_CONTEXT"))
+        val thread = HandlerThread("HandlerThread")
+        thread.start()
 
-        Bugsnag.addOnError { error ->
-            error.addMetadata("test", "redacted", "foo")
-            error.addMetadata("test", "present", "bar")
-            true
-        }
+        Handler(thread.looper).post(Runnable {
+            context.startActivity(Intent("com.bugsnag.android.mazerunner.UPDATE_CONTEXT"))
 
-        Bugsnag.notify(IgnorableException("Ignore me"))
+            Bugsnag.addOnError { error ->
+                error.addMetadata("test", "redacted", "foo")
+                error.addMetadata("test", "present", "bar")
+                true
+            }
 
-        Bugsnag.notify(RuntimeException("LoadConfigException"))
+            flushAllSessions()
+            Thread.sleep(SLEEP_MS)
+
+            Bugsnag.notify(IgnorableException("Ignore me"))
+
+            Bugsnag.notify(RuntimeException("LoadConfigException"))
+        })
     }
 
 }
