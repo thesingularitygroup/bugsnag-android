4c09f6ead35aa165e3f1f3ef2c3a1a5c4c7fc4a4
Alex Moinet <alex@bugsnag.com>
2019-11-12 13:07:49 +0000

    Test immutable config: Ensure maze-runner scenarios can be built

--- mazerunner-scenarios/src/main/java/com/bugsnag/android/mazerunner/IgnorableException.java
+++ mazerunner-scenarios/src/main/java/com/bugsnag/android/mazerunner/IgnorableException.java
@@ -1,6 +1,9 @@
 package com.bugsnag.android.mazerunner;
 
 public class IgnorableException extends Exception {
+
+    public static final long serialVersionUID = 42L;
+
     public IgnorableException(String errorMessage) {
         super(errorMessage);
     }
