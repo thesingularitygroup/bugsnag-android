f3cedb398e169846d0d958ecb472b8bf68b83a08
Alex Moinet <alex@bugsnag.com>
2019-11-13 13:43:22 +0000

    Test immutable config: Add immutable test

---
+++ mazerunner-scenarios/src/main/java/com/bugsnag/android/mazerunner/scenarios/LoadConfigurationImmutableScenario.kt
@@ -0,0 +1,53 @@
+package com.bugsnag.android.mazerunner.scenarios
+
+import com.bugsnag.android.Bugsnag
+import com.bugsnag.android.Configuration
+import com.bugsnag.android.flushAllSessions
+import com.bugsnag.android.mazerunner.IgnorableException
+import com.bugsnag.android.OnError;
+
+import android.content.Context
+import android.content.Intent
+
+import android.os.Handler
+import android.os.HandlerThread
+
+/**
+ * Sends a handled exception to Bugsnag, which does not include session data.
+ */
+internal class LoadConfigurationImmutableScenario(config: Configuration,
+                                        context: Context) : Scenario(config, context) {
+
+    companion object {
+        private const val SLEEP_MS: Long = 300
+    }
+
+    init {
+        config.autoTrackSessions = false
+        config.appVersion = "1.2.3"
+    }
+
+    override fun run() {
+        super.run()
+        val thread = HandlerThread("HandlerThread")
+        thread.start()
+
+        Handler(thread.looper).post(Runnable {
+            context.startActivity(Intent("com.bugsnag.android.mazerunner.UPDATE_CONTEXT"))
+
+            Bugsnag.addOnError(OnError { error ->
+                error.addMetadata("test", "redacted", "foo")
+                error.addMetadata("test", "present", "bar")
+                true
+            })
+
+            flushAllSessions()
+            Thread.sleep(SLEEP_MS)
+
+            Bugsnag.notify(IgnorableException("Ignore me"))
+
+            Bugsnag.notify(RuntimeException("LoadConfigException"))
+        })
+    }
+
+}
